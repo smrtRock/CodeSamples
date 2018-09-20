9578641c1be37bd3435280e6f4cafbd30bd25467
Marcus Fernandez <marcus.fernandez@outlook.com>
2018-09-20 14:55:21 -0400

    added some ts projects

---
+++ TypeScript/Tasks/mytask/HelloWorld.js
@@ -0,0 +1,11 @@
+"use strict";
+var Startup = /** @class */ (function () {
+    function Startup() {
+    }
+    Startup.main = function () {
+        console.log(' Hello World');
+        return 0;
+    };
+    return Startup;
+}());
+Startup.main();
