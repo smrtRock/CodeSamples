e7488d307cde9d1f6e889c00558eefbf3045e380
Smrtrock <smrtrock@outlook.com>
2019-09-30 18:15:51 -0400

    added a export package function and TMDB Genres

---
+++ ChocolateyConfigs/ExportChocolateyPackages.ps1
@@ -0,0 +1,17 @@
+﻿#Put this in Export-Chocolatey.ps1 file and run it:
+
+#Export-Chocolatey.ps1 > packages.config
+
+#You can install the packages using
+
+#choco install packages.config -y
+
+
+
+Write-Output "<?xml version=`"1.0`" encoding=`"utf-8`"?>"
+
+Write-Output "<packages>"
+
+choco list -lo -r -y | % { "   <package id=`"$($_.SubString(0, $_.IndexOf("|")))`" />" }# version=`"$($_.SubString($_.IndexOf("|") + 1))`" />" }
+
+Write-Output "</packages>"
