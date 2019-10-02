d86fa2dca318c1c4109b4f5e083b0fff7556be91
Marcus Fernandez <marcusf@microsoft.com>
2019-10-02 15:12:02 -0700

    Added a corp rollout

--- ChocolateyConfigs/Corp-install.ps1
+++ ChocolateyConfigs/Corp-install.ps1
@@ -4,3 +4,8 @@ choco install .\Corp.config -y
 mkdir C:\g
 Set-Location C:\g
 repo init -u https://msazure.visualstudio.com/One/_git/Manifest
+Set-Location $env:USERPROFILE\Source\repos
+git clone https://github.com/smrtrock/codesamples
+copy $env:USERPROFILE\source\repos\codesamples\Powershell\Microsoft.PowerShell_profile.ps1 C:\Users\marcusf.NORTHAMERICA\Documents\WindowsPowerShell 
+Install-Module posh-git -Force
+Install-Module get-childitemcolor -Force
