8e4c771e432109ec62953b5fdb6c5c3ac0cc66ef
Marcus <smrtrock@outlook.com>
2019-10-04 10:44:27 -0700

    Update Corp-install.ps1

--- ChocolateyConfigs/Corp-install.ps1
+++ ChocolateyConfigs/Corp-install.ps1
@@ -1,11 +1,15 @@
+$repoRoot = "g"
+Set-ExecutionPolicy RemoteSigned -Scope LocalMachine -Force
 cmd /C "\\reddog\public\build\bootstrap\install.cmd" 
 Invoke-WebRequest 'https://chocolatey.org/install.ps1' -UseBasicParsing | Invoke-Expression 
-choco install .\Corp.config -y 
-mkdir C:\g
-Set-Location C:\g
+mkdir "C:\$repoRoot"
+Set-Location "C:\$repoRoot"
 repo init -u https://msazure.visualstudio.com/One/_git/Manifest
 Set-Location $env:USERPROFILE\Source\repos
 git clone https://github.com/smrtrock/codesamples
+choco source enable -n=chocolatey
+choco install $env:USERPROFILE\source\repos\codesamples\ChocolateyConfigs\Corp.config -y
+choco source disable -n=chocolatey
 copy $env:USERPROFILE\source\repos\codesamples\Powershell\Microsoft.PowerShell_profile.ps1 C:\Users\marcusf.NORTHAMERICA\Documents\WindowsPowerShell 
 Install-Module posh-git -Force
 Install-Module get-childitemcolor -Force
