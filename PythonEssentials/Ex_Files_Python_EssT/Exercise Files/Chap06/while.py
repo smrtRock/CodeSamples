03342049322c7162cc19f23377e58fdb154ce5f8
Marcus Fernandez <marcusf@microsoft.com>
2020-04-04 17:03:59 -0400

    Loops

--- PythonEssentials/Ex_Files_Python_EssT/Exercise Files/Chap06/while.py
+++ PythonEssentials/Ex_Files_Python_EssT/Exercise Files/Chap06/while.py
@@ -3,6 +3,13 @@
 
 secret = 'swordfish'
 pw = ''
-
+auth = False
+count = 0
+max_attempt = 5
 while pw != secret:
+    count +=1
+    if count > max_attempt: break
+    if count == 3: continue
     pw = input("What's the secret word? ")
+else:
+    auth = True
