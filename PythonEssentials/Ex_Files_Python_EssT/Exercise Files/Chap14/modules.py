a0228ed939ac61c4eb059cf3e5f8e85a7e5f91a8
Marcus Fernandez <marcusf@microsoft.com>
2020-04-05 11:37:02 -0400

    updated python-fordatascience

--- PythonEssentials/Ex_Files_Python_EssT/Exercise Files/Chap14/modules.py
+++ PythonEssentials/Ex_Files_Python_EssT/Exercise Files/Chap14/modules.py
@@ -2,9 +2,11 @@
 # Copyright 2009-2017 BHG http://bw.org/
 
 import sys
+import os
 
 def main():
     v = sys.version_info
     print('Python version {}.{}.{}'.format(*v))
-
+    o = os.name
+    print(o)
 if __name__ == '__main__': main()
