18335fcd31ef22aecdf5fe28621d2705c689aa38
Marcus Fernandez <marcusf@microsoft.com>
2020-04-06 10:39:57 -0400

    cleaned up path to stuff

--- Python-ForDataScience/Data Preparation/datapreparation-basics.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import pandas as pd
-
-from pandas import Series, DataFrame
-
-series_object = Series(np.arange(8), index=['row 1', 'row 2', 'row 3', 'row 4', 'row 5', 'row 6', 'row7', 'row 8'])
-series_object['row 7']
