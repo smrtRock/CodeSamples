89bb058601732468d019dbc4661cf416beca77f9
Marcus Fernandez <marcusf@microsoft.com>
2020-04-06 08:40:05 -0400

    updated Python for data science

--- Python-ForDataScience/Data Preparation/datapreparation-basics.py
+++ Python-ForDataScience/Data Preparation/datapreparation-basics.py
@@ -1,2 +1,7 @@
-import platform
-print('This is pything version {}'.format(platform.python_version()))
+import numpy as np
+import pandas as pd
+
+from pandas import Series, DataFrame
+
+series_object = Series(np.arange(8), index=['row 1', 'row 2', 'row 3', 'row 4', 'row 5', 'row 6', 'row7', 'row 8'])
+series_object['row 7']
